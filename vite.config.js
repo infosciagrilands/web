e110203a7e23801d0839000046569c96db61c944
Kazuyuki Doi <doi@DoiMacBookPro14.local>
2026-02-09 16:34:34 +0900

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     // GitHub Pagesのリポジトリ名に合わせてパスを設定します
-    base: '/info-sci-agri-togo-test-2026-02/',
+    base: '/web/',
     build: {
         outDir: 'dist',
     }
